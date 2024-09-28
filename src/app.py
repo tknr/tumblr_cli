ee8f9271b8dd1f42c03abfef4a8373c388e1452a
tknr <geishagirlskoume@gmail.com>
2024-09-29 00:35:38 +0900

    u

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 import pytumblr2
 import yaml
+import argparse
+
+parser = argparse.ArgumentParser(description='tumblr cli')
+parser.add_argument('-v', '--verbose',action='store_true')
+args = parser.parse_args()
 
 with open('config/config.yml', 'r') as filehandle:
     config = yaml.safe_load(filehandle)
-
-print(config)
+if args.verbose:
+    print(config)
 
 client = pytumblr2.TumblrRestClient(
     config['consumer_key'],
@@ -12,6 +17,6 @@ client = pytumblr2.TumblrRestClient(
     config['oauth_token'],
     config['oauth_secret']
 )
-
-print(client.info())
+if args.verbose:
+    print(client.info())
 
